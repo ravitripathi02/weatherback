21485c07f2b769d240e48816026713d513afdd2f
ravitripathi02 <91239605+ravitripathi02@users.noreply.github.com>
2023-07-24 09:58:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-// OpenWeather API Key - Replace 'YOUR_API_KEY' with your actual API key
+
 const apiKey = "eec90fb4a0b54f4444192b7f582ed1e8";
 const weatherAPIBaseUrl = "https://api.openweathermap.org/data/2.5/weather";
 const baseUrl="https://weatherback-seven.vercel.app/"
-// Weather endpoint
+
 app.post('/', async (req, res) => {
   try {
     const { location } = req.body;
@@ -29,7 +29,7 @@ async function getWeatherData(location) {
     params: {
       q: location,
       appid: apiKey,
-      units: "metric", // Use 'imperial' for Fahrenheit
+      units: "metric", 
     },
   });
   const weatherData = {
